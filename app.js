cea72a7c065a4f1cdfb69fd18a5d973f676de855
Denis <dpaniotov321@gmail.com>
2022-11-12 22:42:38 +0200

    added script for google maps and custom select

---
+++ app.js
@@ -0,0 +1,192 @@
+// Google Map
+function initMap() {
+  const place = { lat: 43.452967514489806, lng: -80.51338045992702 };
+  const stylesArray = [
+    {
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#f5f5f5',
+        },
+      ],
+    },
+    {
+      elementType: 'labels.icon',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#616161',
+        },
+      ],
+    },
+    {
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {
+          color: '#f5f5f5',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#bdbdbd',
+        },
+      ],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#eeeeee',
+        },
+      ],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#757575',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#e5e5e5',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#9e9e9e',
+        },
+      ],
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#ffffff',
+        },
+      ],
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#757575',
+        },
+      ],
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#dadada',
+        },
+      ],
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#616161',
+        },
+      ],
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#9e9e9e',
+        },
+      ],
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#e5e5e5',
+        },
+      ],
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#eeeeee',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#c9c9c9',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#9e9e9e',
+        },
+      ],
+    },
+  ];
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: place,
+    styles: stylesArray,
+  });
+  const marker = new google.maps.Marker({
+    position: place,
+    map: map,
+    icon: './img/carbon_location-filled.svg',
+  });
+  const info = new google.maps.InfoWindow({
+    content:
+      '<h2 class="title">Voodoo</h2><p class="address">137 Glasgow St., Unit 115<br> Kitchener, ON N2G 4X8<br>Ukraine</p><div class="contacts"><img src="./img/phone.svg" class="phone-icon"><a class="phone" href="telto:1-800-480-9597">1-800-480-9597</a><img src="./img/mail.png" class="email-icon"><a class="mail" href="mailto:info@voodoo.com">info@voodoo.com</a></div>',
+  });
+  marker.addListener('click', function () {
+    info.open(map, marker);
+  });
+}
+
+window.initMap = initMap;
+
+// Select
+const selects = document.querySelectorAll('.contacts-form__select');
+
+selects.forEach((select) => {
+  select.addEventListener('click', () => {
+    select.classList.toggle('active');
+  });
+});
